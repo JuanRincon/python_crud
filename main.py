d393c24c1d342458891c47f3bd968c43e089e775
Juan <juancarive@gmail.com>
2023-02-04 11:55:44 -0500

    Se añadió la coma al final de un cliente nuevo como una función

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ clients = 'pablo,ricardo,'
 
 def create_client(client_name):
     global clients
-    clients += (client_name)
+
+    clients += client_name
+    _add_coma()
+
+
+def _add_coma():
+    global clients
+
+    clients += ","
+
 
 if __name__ == '__main__':
     create_client('David')

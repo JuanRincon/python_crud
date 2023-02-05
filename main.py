23601a115354516e6620438cbc528b1e9457b90f
Juan <juancarive@gmail.com>
2023-02-04 22:47:29 -0500

    Se cambiaron los valores estaticos por input del usuario. También si hay un cliente repetido no lo ingresa

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ clients = 'pablo,ricardo,'
 
 def create_client(client_name):
     global clients
-
-    clients += client_name
-    _add_coma()
+    
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        print('Client already is in the client\'s list')
 
 
 def list_clients():
@@ -15,16 +18,33 @@ def list_clients():
     print(clients)
     
 
-def _add_coma():
+def _add_comma():
     global clients
 
     clients += ","
 
 
+def _print_welcome():
+    print('WELCOME TO PLATZI VENTAS')
+    print('*' * 50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[D]elete client')
+
+
 if __name__ == '__main__':
-    list_clients()
+    _print_welcome()
 
-    create_client('David')
+    command = input()
 
-    list_clients()
+    if command == 'C':
+        client_name = input('What is the client name? ')
+        create_client(client_name)
+        list_clients()
+    elif command == 'D':
+        pass
+    else:
+        print('Invalid command')
+
+
 

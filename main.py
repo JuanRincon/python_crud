0ef4a37b7110d6418b93eaaa62af1155035fe552
Juan <juancarive@gmail.com>
2023-02-09 16:48:54 -0500

    Se añade funcion update, input update_client, y la funcion minusculas al elegir las opciones del CRUD

--- main.py
+++ main.py
@@ -18,6 +18,14 @@ def list_clients():
     print(clients)
     
 
+def update_client(client_name, updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', updated_client_name + ',')
+    else:
+        print('Client is not in clients list')
+
 def _add_comma():
     global clients
 
@@ -27,19 +35,30 @@ def _add_comma():
 def _print_welcome():
     print('WELCOME TO PLATZI VENTAS')
     print('*' * 50)
-    print('What would you like to do today?')
+    print('What would you like to do today? ')
     print('[C]reate client')
+    print('[U]pdate client')
     print('[D]elete client')
+
+
+def _get_client_name():
+    return input('What is the client name? ')
 
 
 if __name__ == '__main__':
     _print_welcome()
 
     command = input()
+    command = command.upper()
 
     if command == 'C':
-        client_name = input('What is the client name? ')
+        client_name = _get_client_name() 
         create_client(client_name)
+        list_clients()
+    elif command == 'U':
+        client_name = _get_client_name() 
+        updated_client_name = input('What is the updated client name ')
+        update_client(client_name, updated_client_name)
         list_clients()
     elif command == 'D':
         pass

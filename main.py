5a72bf567a334303383cfbe894538ca2c5650352
Juan <juancarive@gmail.com>
2023-02-10 11:37:30 -0500

    Se agrega función Delete para completar el CRUD

--- main.py
+++ main.py
@@ -25,6 +25,15 @@ def update_client(client_name, updated_client_name):
         clients = clients.replace(client_name + ',', updated_client_name + ',')
     else:
         print('Client is not in clients list')
+        
+
+def delete_client(client_name):
+    global clients
+
+    if client_name in clients:
+       clients = clients.replace(client_name + ',', '')
+    else:
+        print('Client is not in clients list')
 
 def _add_comma():
     global clients
@@ -61,7 +70,9 @@ if __name__ == '__main__':
         update_client(client_name, updated_client_name)
         list_clients()
     elif command == 'D':
-        pass
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
     else:
         print('Invalid command')
 

46594e6454f6717c17d2c279b02d7299640953d2
Juan <juancarive@gmail.com>
2023-02-13 12:26:58 -0500

    Anotaciones a algunos comandos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import sys        # Importa librería para su uso en sys.exit()
 
 clients = 'pablo,ricardo,'
 
@@ -9,7 +9,7 @@ def create_client(client_name):
         clients += client_name
         _add_comma()
     else:
-        print('Client already is in the client\'s list')
+        print('Client already is in the client\'s list')  # El \ antes de ' permite ingresarlo  sin cerrar el comentario
 
 
 def list_clients():
@@ -21,8 +21,8 @@ def list_clients():
 def update_client(client_name, updated_client_name):
     global clients
 
-    if client_name in clients:
-        clients = clients.replace(client_name + ',', updated_client_name + ',')
+    if client_name in clients:                        # in se encarga de validar que el contenido de la variable client_name esté incluido en el contenido de la variable clients
+        clients = clients.replace(client_name + ',', updated_client_name + ',')     # replace es un método para remplazar el contenido de la variable que lo antepone al punto, primero se coloca el valor a reemplazar seguido por el contenido que lo reemplaza
     else:
         print('Client is not in clients list')
         
@@ -37,11 +37,11 @@ def delete_client(client_name):
         
 
 def search_client(client_name):
-    clients_list = clients.split(',')
+    clients_list = clients.split(',')     # El método split es para dividir el contenido que tienen la variabla entre contenidos más pequeños cada que se encuentre el valor que se le especifica en este caso la coma
 
-    for client in clients_list:
-        if client != client_name:
-            continue
+    for client in clients_list:     # client es una variable que se crea acá para ejecutar el ciclo for
+        if client != client_name:   
+            continue                # continue sirve para salir anticipadamente del ciclo, e indica que no ejecute nada más dentro del bloque del for loop y vete a la siguiente iteración, a diferencia del break que lo que dice es salir completamente de la iteración
         else:
             return True
 
@@ -64,19 +64,19 @@ def _print_welcome():
 
 
 def _get_client_name():
-    client_name = None
+    client_name = None     # None deja vacia la variable
 
-    while not client_name:
+    while not client_name:    # while not  dice que mientras no haya la variable ingresela
         client_name = input('What is the client name? ')
 
-        if client_name == 'exit':
-            client_name = None
-            break
+        if client_name == 'exit':        # si ingresan exit en el lugar del nombre del cliente (variable) salir
+            client_name = None           # se ingresa nuevamente el None para borrar el exit de la variable antes de salir
+            break                        # el break sirve para salir del loop while, a diferencia del continue que se explicó más arriba
 
-    if not client_name:
+    if not client_name:                  # este condicional sirve para dejar de ejecutar el programa
             sys.exit()
 
-    return client_name
+    return client_name                   # este return permite que la función regrese o devuelva el nombre del cliente que es la variable
 
 
 if __name__ == '__main__':
@@ -107,7 +107,7 @@ if __name__ == '__main__':
         if found:
             print('The clienty is in the client\'s list')
         else:
-            print('The client: {} is not in our clinet\'s list' .format(client_name))
+            print('The client: {} is not in our clinet\'s list' .format(client_name))   # las {} es un placeholder que permite poner nuevos valores con posterioridad, lo que permite adicionarles es el .format(variable_que_tiene_los_valores_a_adicionar)
     else:
         print('Invalid command')
 

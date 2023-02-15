8e6f5ec7e8e93d1516594ed6c6e9c6920ce59c4c
Juan <juancarive@gmail.com>
2023-02-15 16:44:10 -0500

    Se cambia la estructura de datos de string a listas

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
-import sys        # Importa librería para su uso en sys.exit()
+ 
+""" Notas de cambios main.py
+ 
+ cambio de clients como string por la estructura de lista
+ Eliminación de función _add_comma()
+ reemplazo de clients += client_name por clients.append(client_name)
+ sustitución de print(clients en la función list_clients() por print('{}: {}'.format(idx, client) y poniendo antes el iterador for idx, client in     enumerate(clients):)
+ Sustitución de clients = clients.replace(client_name + ',', updated_client_name + ',')  en la función update_client por                             index = clients.index(client_name)
+ clients[index] = updated_name
+ Sustitución de clients = clients.replace(client_name + ',', '') en la función delete_cliente por clients.remove(client_name)
+ Remoción de clients_list = clients.split(',') en función search_client y sustitución de for client in clients_list: por for client in clients:
+ Se elimina la función _add_comma()
+"""
 
-clients = 'pablo,ricardo,'
+import sys        
+
+clients = ['pablo','ricardo']
 
 def create_client(client_name):
     global clients
     
     if client_name not in clients:
-        clients += client_name
-        _add_comma()
+       clients.append(client_name)
     else:
-        print('Client already is in the client\'s list')  # El \ antes de ' permite ingresarlo  sin cerrar el comentario
+        print('Client already is in the client\'s list')  
 
 
 def list_clients():
-    global clients
-
-    print(clients)
+    for idx, client in enumerate(clients):
+        print('{}: {}'.format(idx, client))
     
 
 def update_client(client_name, updated_client_name):
     global clients
 
-    if client_name in clients:                        # in se encarga de validar que el contenido de la variable client_name esté incluido en el contenido de la variable clients
-        clients = clients.replace(client_name + ',', updated_client_name + ',')     # replace es un método para remplazar el contenido de la variable que lo antepone al punto, primero se coloca el valor a reemplazar seguido por el contenido que lo reemplaza
+    if client_name in clients:                        
+       index = clients.index(client_name)
+       clients[index] = updated_name
     else:
         print('Client is not in clients list')
         
@@ -31,25 +44,17 @@ def delete_client(client_name):
     global clients
 
     if client_name in clients:
-       clients = clients.replace(client_name + ',', '')
+        clients.remove(client_name)   
     else:
         print('Client is not in clients list')
         
 
 def search_client(client_name):
-    clients_list = clients.split(',')     # El método split es para dividir el contenido que tienen la variabla entre contenidos más pequeños cada que se encuentre el valor que se le especifica en este caso la coma
-
-    for client in clients_list:     # client es una variable que se crea acá para ejecutar el ciclo for
+    for client in clients:     
         if client != client_name:   
-            continue                # continue sirve para salir anticipadamente del ciclo, e indica que no ejecute nada más dentro del bloque del for loop y vete a la siguiente iteración, a diferencia del break que lo que dice es salir completamente de la iteración
+            continue                
         else:
             return True
-
-
-def _add_comma():
-    global clients
-
-    clients += ","
 
 
 def _print_welcome():
@@ -64,19 +69,19 @@ def _print_welcome():
 
 
 def _get_client_name():
-    client_name = None     # None deja vacia la variable
+    client_name = None     
 
-    while not client_name:    # while not  dice que mientras no haya la variable ingresela
+    while not client_name:    
         client_name = input('What is the client name? ')
 
-        if client_name == 'exit':        # si ingresan exit en el lugar del nombre del cliente (variable) salir
-            client_name = None           # se ingresa nuevamente el None para borrar el exit de la variable antes de salir
-            break                        # el break sirve para salir del loop while, a diferencia del continue que se explicó más arriba
+        if client_name == 'exit':     
+            client_name = None         
+            break                       
 
-    if not client_name:                  # este condicional sirve para dejar de ejecutar el programa
+    if not client_name:                  
             sys.exit()
 
-    return client_name                   # este return permite que la función regrese o devuelva el nombre del cliente que es la variable
+    return client_name                   
 
 
 if __name__ == '__main__':
@@ -107,7 +112,7 @@ if __name__ == '__main__':
         if found:
             print('The clienty is in the client\'s list')
         else:
-            print('The client: {} is not in our clinet\'s list' .format(client_name))   # las {} es un placeholder que permite poner nuevos valores con posterioridad, lo que permite adicionarles es el .format(variable_que_tiene_los_valores_a_adicionar)
+            print('The client: {} is not in our clinet\'s list' .format(client_name))   
     else:
         print('Invalid command')
 
